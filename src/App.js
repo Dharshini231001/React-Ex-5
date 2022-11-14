05195fce853964f64abcb28d18019d8581d8abe5
Dharshini <95228786+Dharshini231001@users.noreply.github.com>
2022-11-14 09:20:11 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import React from 'react';
+class App extends React.Component{
+  render(){
+    return(
+      <div><center>
+       <Button text="Welcome to Reactjs(Ex-5)"/></center><br></br>
+      </div>
+    )   }
+}
+class Button extends React.Component{
+  state={
+    textflag:false,
+  }
+  ToggleButton(){
+    this.setState(
+      {textflag:!this.state.textflag}
+    );
+  }
+  render(){
+    return(
+      <div>
+        <center><button onClick={()=>this.ToggleButton()}>
+          {this.state.textflag===false? "Hide":"Display"}
+        </button></center>
+        {!this.state.textflag&&this.props.text}
+      </div>
+    )
+  }
+}
+export default App;
+
+
